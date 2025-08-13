ff9a4238b593ac08ea2493bba5e88e469a091565
Rishi Kumar <rishikumar27203@gmail.com>
2025-08-13 18:03:30 +0530

    if else switch break

---
+++ If-Else_Switch-break/Greeting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        if (button == 1) {
+            System.out.println("Hello");
+        } else if (button == 2) {
+            System.out.println("Namaste");
+        }else{
+            System.out.println("Bounju");
+        }
+    }
+}
