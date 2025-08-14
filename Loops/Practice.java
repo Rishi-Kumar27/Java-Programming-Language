ea803631ead0b5e0231601f38606ff0f667cf22b
Rishi Kumar <rishikumar27203@gmail.com>
2025-08-14 18:10:13 +0530

    loops

---
+++ Loops/Practice.java
@@ -0,0 +1,15 @@
+//Print the sum of first n natural numbers
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+    
+}
---
+++ Loops/PracticeQ2.java
@@ -0,0 +1,13 @@
+// Print the table of a number input by the user
+import java.util.*;
+
+
+public class PracticeQ2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n * i);
+        }
+    }
+}
